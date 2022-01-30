59587188f9ee367bfb45a5c4921a9422bda1ddbf
Jayden <yujonglee@g.skku.edu>
2022-01-30 16:13:53 +0900

    Remove unintentionally added test)

--- src/math.zig
+++ src/math.zig
@@ -1,12 +1,7 @@
 const std = @import("std");
-const testing = std.testing;
 
 extern fn print(i32) void;
 
 export fn add(a: i32, b: i32) void {
     print(a + b);
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
 }
